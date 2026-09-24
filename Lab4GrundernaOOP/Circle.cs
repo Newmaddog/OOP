33c2ccf58d07f201024694061c7883406c2b35cf
Newmaddog <erico.l.a.pizarro@hotmail.com>
2026-09-24 23:23:40 +0200

    Uppgift 4 klart

---
+++ Lab4GrundernaOOP/Circle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab4GrundernaOOP
+{
+    internal class Circle
+    {
+        float Radius;
+
+        public Circle(float radius)
+        {
+            Radius = radius;
+        }
+
+        internal float GetArea()
+        {
+            float area;
+            area = Radius * Radius * ((float)Math.PI);
+            return area;
+        }
+    }
+}
---
+++ Lab4GrundernaOOP/Program.cs
@@ -0,0 +1,16 @@
+﻿//Namn: Erico Leonardo Alexander Pizarro Gutierrez
+//Klass: NET26
+namespace Lab4GrundernaOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle(5);
+            Circle BigCircle = new Circle(6);
+
+            Console.WriteLine($"The little circle's area is: {circle.GetArea()}");
+            Console.WriteLine($"The big circle's area is: {BigCircle.GetArea()}");
+        }
+    }
+}
